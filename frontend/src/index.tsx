36c019f5dd684ece759eff13ff65a5eb861f3593
elianiva <dicha.arkana03@gmail.com>
2022-02-05 17:25:36 +0700

    feat: setup router and alias

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,13 @@
 import { render } from "solid-js/web";
-
-import "./index.css";
+import { Router } from "solid-app-router";
 import App from "./App";
+import "./index.css";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById("root") as HTMLElement
+);
---
+++ frontend/src/pages/Overview.tsx
@@ -0,0 +1,8 @@
+export default function OverviewPage() {
+  return (
+    <div>
+      <h1>Overview</h1>
+      <p>This is the overview page.</p>
+    </div>
+  );
+}
