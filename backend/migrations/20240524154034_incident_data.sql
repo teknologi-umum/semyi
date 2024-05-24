e5e373aa62239805641a3ffa63d4405c3e2eaf45
takadev15 <daffa.hp.dh@gmail.com>
2024-05-24 23:09:36 +0700

    feat(backend): add migrations schema for incident reporting data

---
+++ backend/migrations/20240524154034_incident_data.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE incident_data (
+    incident_id VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    severity SMALLINT NOT NULL,
+    status SMALLINT NOT NULL,
+    created_by VARCHAR(255) NOT NULL
+);
+
+CREATE INDEX incident_data_incident_id_idx ON incident_data (incident_id, timestamp);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE incident_data;
+-- +goose StatementEnd
