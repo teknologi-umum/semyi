92f3ab8f6cdf7068bc5260f6e830e5b8e3798268
Reinaldy Rafli <github@reinaldyrafli.com>
2025-04-04 16:21:56 +0700

    fix(migrations): state primary key inside of the 'CREATE TABLE' invocation

--- backend/migrations/20240523085502_monitor_historical.sql
+++ backend/migrations/20240523085502_monitor_historical.sql
@@ -4,30 +4,27 @@ CREATE TABLE IF NOT EXISTS monitor_historical (
     monitor_id VARCHAR(255) NOT NULL,
     status SMALLINT NOT NULL,
     latency INTEGER NOT NULL DEFAULT 0,
-    timestamp TIMESTAMP NOT NULL DEFAULT NOW()
+    timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (monitor_id, timestamp)
 );
-
-CREATE INDEX IF NOT EXISTS monitor_historical_monitor_id_idx ON monitor_historical (monitor_id);
 
 CREATE TABLE IF NOT EXISTS monitor_historical_hourly_aggregate (
     timestamp TIMESTAMP NOT NULL,
     monitor_id VARCHAR(255) NOT NULL,
     status SMALLINT NOT NULL,
     latency INTEGER NOT NULL DEFAULT 0,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (monitor_id, timestamp)
 );
-
-CREATE UNIQUE INDEX IF NOT EXISTS monitor_historical_hourly_aggregate_monitor_id_timestamp_idx ON monitor_historical_hourly_aggregate (monitor_id, timestamp);
 
 CREATE TABLE IF NOT EXISTS monitor_historical_daily_aggregate (
     timestamp TIMESTAMP NOT NULL,
     monitor_id VARCHAR(255) NOT NULL,
     status SMALLINT NOT NULL,
     latency INTEGER NOT NULL DEFAULT 0,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (monitor_id, timestamp)
 );
-
-CREATE UNIQUE INDEX IF NOT EXISTS monitor_historical_daily_aggregate_monitor_id_timestamp_idx ON monitor_historical_daily_aggregate (monitor_id, timestamp);
 -- +goose StatementEnd
 
 -- +goose Down
