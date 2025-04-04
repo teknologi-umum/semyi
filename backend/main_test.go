ffbd81b9b3efec7c89ee48ffe3a854e1f69914e9
Reinaldy Rafli <github@reinaldyrafli.com>
2025-04-04 15:50:18 +0700

    ci: test backend against clickhouse
    I did not vibe code this

--- backend/main_test.go
+++ backend/main_test.go
@@ -3,11 +3,16 @@ package main_test
 import (
 	"context"
 	"database/sql"
+	"database/sql/driver"
 	"os"
+	"strings"
 	"testing"
+	"time"
 
 	main "semyi"
 
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/marcboeker/go-duckdb/v2"
 	_ "github.com/marcboeker/go-duckdb/v2"
 	"github.com/rs/zerolog/log"
 )
@@ -17,12 +22,31 @@ var database *sql.DB
 func TestMain(m *testing.M) {
 	// Setup
 	var err error
-	database, err = sql.Open("duckdb", "")
-	if err != nil {
-		log.Error().Err(err).Msg("failed to open database connection")
-		os.Exit(1)
-		return
+	databasePath := os.Getenv("DATABASE_URL")
+	var connector driver.Connector
+	if strings.HasPrefix(databasePath, "clickhouse://") || strings.HasPrefix(databasePath, "http://") {
+		clickHouseOptions, err := clickhouse.ParseDSN(databasePath)
+		if err != nil {
+			log.Error().Err(err).Msg("failed to parse clickhouse DSN")
+			os.Exit(1)
+			return
+		}
+
+		connector = clickhouse.Connector(clickHouseOptions)
+	} else {
+		connector, err = duckdb.NewConnector(databasePath, func(execer driver.ExecerContext) error {
+			return nil
+		})
+		if err != nil {
+			log.Error().Err(err).Msg("failed to create duckdb connector")
+			os.Exit(1)
+			return
+		}
 	}
+
+	database = sql.OpenDB(connector)
+	database.SetConnMaxIdleTime(time.Minute)
+	database.SetConnMaxLifetime(time.Minute * 10)
 
 	// Migrate database
 	err = main.Migrate(database, context.Background(), true)
