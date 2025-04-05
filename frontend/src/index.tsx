3550657f553c9fbe0b939ec07a8c22b8d13d5fb0
elianiva <51877647+elianiva@users.noreply.github.com>
2025-04-05 10:37:29 +0700

    fix: use Number namespace for parsing floats

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -13,19 +13,19 @@ import { Route, Router } from "@solidjs/router";
 Sentry.init({
   dsn: import.meta.env.VITE_FRONTEND_SENTRY_DSN,
   integrations: [solidRouterBrowserTracingIntegration(), Sentry.replayIntegration()],
-  sampleRate: parseFloat(import.meta.env.VITE_FRONTEND_SENTRY_SAMPLE_RATE ?? 1.0),
+  sampleRate: Number.parseFloat(import.meta.env.VITE_FRONTEND_SENTRY_SAMPLE_RATE ?? 1.0),
   // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
   // We recommend adjusting this value in production
   // Learn more at
   // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
-  tracesSampleRate: parseFloat(import.meta.env.VITE_FRONTEND_SENTRY_TRACES_SAMPLE_RATE ?? 1.0),
+  tracesSampleRate: Number.parseFloat(import.meta.env.VITE_FRONTEND_SENTRY_TRACES_SAMPLE_RATE ?? 1.0),
   // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
   tracePropagationTargets: ["localhost", window.location.origin],
   // Capture Replay for 10% of all sessions, plus 100% of sessions with an error
   // Learn more at
   // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
-  replaysSessionSampleRate: parseFloat(import.meta.env.VITE_FRONTEND_SENTRY_REPLAYS_SESSION_SAMPLE_RATE ?? 0.05),
-  replaysOnErrorSampleRate: parseFloat(import.meta.env.VITE_FRONTEND_SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE ?? 0.5),
+  replaysSessionSampleRate: Number.parseFloat(import.meta.env.VITE_FRONTEND_SENTRY_REPLAYS_SESSION_SAMPLE_RATE ?? 0.05),
+  replaysOnErrorSampleRate: Number.parseFloat(import.meta.env.VITE_FRONTEND_SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE ?? 0.5),
 });
 
 // Wrap Solid Router to collect meaningful performance data on route changes
