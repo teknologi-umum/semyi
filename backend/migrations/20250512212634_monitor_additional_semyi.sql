85cac35bee3f543687ee89d53ad5eba79f3ccc91
Reinaldy Rafli <github@reinaldyrafli.com>
2025-05-17 11:54:54 +0700

    fix(backend): should be able to execute migrations multiple times

--- backend/migrations/20250512212634_monitor_additional_semyi.sql
+++ backend/migrations/20250512212634_monitor_additional_semyi.sql
@@ -1,41 +1,41 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE monitor_historical ADD COLUMN additional_message TEXT;
-ALTER TABLE monitor_historical ADD COLUMN http_protocol VARCHAR(255);
-ALTER TABLE monitor_historical ADD COLUMN tls_version VARCHAR(255);
-ALTER TABLE monitor_historical ADD COLUMN tls_cipher VARCHAR(255);
-ALTER TABLE monitor_historical ADD COLUMN tls_expiry TIMESTAMP;
+ALTER TABLE monitor_historical ADD COLUMN IF NOT EXISTS additional_message TEXT;
+ALTER TABLE monitor_historical ADD COLUMN IF NOT EXISTS http_protocol VARCHAR(255);
+ALTER TABLE monitor_historical ADD COLUMN IF NOT EXISTS tls_version VARCHAR(255);
+ALTER TABLE monitor_historical ADD COLUMN IF NOT EXISTS tls_cipher VARCHAR(255);
+ALTER TABLE monitor_historical ADD COLUMN IF NOT EXISTS tls_expiry TIMESTAMP;
 
-ALTER TABLE monitor_historical_daily_aggregate ADD COLUMN additional_message TEXT;
-ALTER TABLE monitor_historical_daily_aggregate ADD COLUMN http_protocol VARCHAR(255);
-ALTER TABLE monitor_historical_daily_aggregate ADD COLUMN tls_version VARCHAR(255);
-ALTER TABLE monitor_historical_daily_aggregate ADD COLUMN tls_cipher VARCHAR(255);
-ALTER TABLE monitor_historical_daily_aggregate ADD COLUMN tls_expiry TIMESTAMP;
+ALTER TABLE monitor_historical_daily_aggregate ADD COLUMN IF NOT EXISTS additional_message TEXT;
+ALTER TABLE monitor_historical_daily_aggregate ADD COLUMN IF NOT EXISTS http_protocol VARCHAR(255);
+ALTER TABLE monitor_historical_daily_aggregate ADD COLUMN IF NOT EXISTS tls_version VARCHAR(255);
+ALTER TABLE monitor_historical_daily_aggregate ADD COLUMN IF NOT EXISTS tls_cipher VARCHAR(255);
+ALTER TABLE monitor_historical_daily_aggregate ADD COLUMN IF NOT EXISTS tls_expiry TIMESTAMP;
 
-ALTER TABLE monitor_historical_hourly_aggregate ADD COLUMN additional_message TEXT;
-ALTER TABLE monitor_historical_hourly_aggregate ADD COLUMN http_protocol VARCHAR(255);
-ALTER TABLE monitor_historical_hourly_aggregate ADD COLUMN tls_version VARCHAR(255);
-ALTER TABLE monitor_historical_hourly_aggregate ADD COLUMN tls_cipher VARCHAR(255);
-ALTER TABLE monitor_historical_hourly_aggregate ADD COLUMN tls_expiry TIMESTAMP;
+ALTER TABLE monitor_historical_hourly_aggregate ADD COLUMN IF NOT EXISTS additional_message TEXT;
+ALTER TABLE monitor_historical_hourly_aggregate ADD COLUMN IF NOT EXISTS http_protocol VARCHAR(255);
+ALTER TABLE monitor_historical_hourly_aggregate ADD COLUMN IF NOT EXISTS tls_version VARCHAR(255);
+ALTER TABLE monitor_historical_hourly_aggregate ADD COLUMN IF NOT EXISTS tls_cipher VARCHAR(255);
+ALTER TABLE monitor_historical_hourly_aggregate ADD COLUMN IF NOT EXISTS tls_expiry TIMESTAMP;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE monitor_historical DROP COLUMN additional_message;
-ALTER TABLE monitor_historical DROP COLUMN http_protocol;
-ALTER TABLE monitor_historical DROP COLUMN tls_version;
-ALTER TABLE monitor_historical DROP COLUMN tls_cipher;
-ALTER TABLE monitor_historical DROP COLUMN tls_expiry;
+ALTER TABLE monitor_historical DROP COLUMN IF EXISTS additional_message;
+ALTER TABLE monitor_historical DROP COLUMN IF EXISTS http_protocol;
+ALTER TABLE monitor_historical DROP COLUMN IF EXISTS tls_version;
+ALTER TABLE monitor_historical DROP COLUMN IF EXISTS tls_cipher;
+ALTER TABLE monitor_historical DROP COLUMN IF EXISTS tls_expiry;
 
-ALTER TABLE monitor_historical_daily_aggregate DROP COLUMN additional_message;
-ALTER TABLE monitor_historical_daily_aggregate DROP COLUMN http_protocol;
-ALTER TABLE monitor_historical_daily_aggregate DROP COLUMN tls_version;
-ALTER TABLE monitor_historical_daily_aggregate DROP COLUMN tls_cipher;
-ALTER TABLE monitor_historical_daily_aggregate DROP COLUMN tls_expiry;
+ALTER TABLE monitor_historical_daily_aggregate DROP COLUMN IF EXISTS additional_message;
+ALTER TABLE monitor_historical_daily_aggregate DROP COLUMN IF EXISTS http_protocol;
+ALTER TABLE monitor_historical_daily_aggregate DROP COLUMN IF EXISTS tls_version;
+ALTER TABLE monitor_historical_daily_aggregate DROP COLUMN IF EXISTS tls_cipher;
+ALTER TABLE monitor_historical_daily_aggregate DROP COLUMN IF EXISTS tls_expiry;
 
-ALTER TABLE monitor_historical_hourly_aggregate DROP COLUMN additional_message;
-ALTER TABLE monitor_historical_hourly_aggregate DROP COLUMN http_protocol;
-ALTER TABLE monitor_historical_hourly_aggregate DROP COLUMN tls_version;
-ALTER TABLE monitor_historical_hourly_aggregate DROP COLUMN tls_cipher;
-ALTER TABLE monitor_historical_hourly_aggregate DROP COLUMN tls_expiry;
+ALTER TABLE monitor_historical_hourly_aggregate DROP COLUMN IF EXISTS additional_message;
+ALTER TABLE monitor_historical_hourly_aggregate DROP COLUMN IF EXISTS http_protocol;
+ALTER TABLE monitor_historical_hourly_aggregate DROP COLUMN IF EXISTS tls_version;
+ALTER TABLE monitor_historical_hourly_aggregate DROP COLUMN IF EXISTS tls_cipher;
+ALTER TABLE monitor_historical_hourly_aggregate DROP COLUMN IF EXISTS tls_expiry;
 -- +goose StatementEnd
